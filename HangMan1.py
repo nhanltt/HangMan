5e3f09c202fd7917a395dad85944f6709b5470ab
nhanltt <61408879+nhanltt@users.noreply.github.com>
2020-11-18 10:55:35 +0700

    Update HangMan1.py

--- HangMan1.py
+++ HangMan1.py
@@ -1,7 +1,7 @@
 # Program: HANGMAN written by Nhan Thi Thanh Le, 18th Nov 2020
 import random
 words = ['python', 'java', 'kotlin', 'javascript']
-n = random.randint(0,len(words)-1)
+n = random.randint(0, len(words)-1)
 word = words[n]
 l = len(word)
 # Print information of the guessed word
@@ -13,18 +13,18 @@ times = 0
 guessed = []
 checkword = ['-' for i in range(l)]
 # Write main program - using while
-while not win and times<8:
+while not win and times<8 :
     m = input("Input the letter: > ")
     # The letter in word
     if m in word:
-        #The letter wasn't be guessed before
+        # The letter wasn't be guessed before
         if m not in guessed:
             guessed.append(m)
             for i in range(l):
-                if word[i] == m :
+                if word[i] == m:
                     checkword[i] = m
             for i in checkword:
-                print(i,end='')
+                print(i, end='')
             print()
             # The player win
             if checkword.count('-') == 0:
